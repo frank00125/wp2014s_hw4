07b8bd49f38a5886dd94a53e536286b2c3c906b3
frank00125 <jhihwei.su@gmail.com>
2014-06-04 23:08:06 +0800

    }

--- js/script.js
+++ js/script.js
@@ -95,44 +95,44 @@ window.fbAsyncInit = function () {//facebook init
     var isDragging = false;//拖拉
 
     function handleMouseDown(e){//滑鼠按下的函數
-      canMouseX = parseInt(e.clientX-offsetX);//抓滑鼠游標X
-      canMouseY = parseInt(e.clientY-offsetY);//抓滑鼠游標y
-      // set the drag flag
-      isDragging=true;//宣告拖拉變數
+		canMouseX = parseInt(e.clientX-offsetX);//抓滑鼠游標X
+		canMouseY = parseInt(e.clientY-offsetY);//抓滑鼠游標y
+		var profileIMG = document.getElementById("preview");//抓html裡預載入的照片
+		profileIMG.crossOrigin = "Anonymous"; // 這務必要做，為了讓Facebook的照片能夠crossdomain傳入到你的頁面，CORS Policy請參考https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image 
+		//canvas.width = profileIMG.width;//設定canvas的大小需符合profileimg的大小
+		//canvas.height = profileIMG.height;
+		ctx.drawImage(profileIMG,canMouseX,canMouseY);//從XY軸0，0值開始畫如profileimg
+		// set the drag flag
+		isDragging=true;//宣告拖拉變數
     }
 
     function handleMouseUp(e){//滑鼠放掉的函數
-      canMouseX = parseInt(e.clientX-offsetX);
-      canMouseY = parseInt(e.clientY-offsetY);
-      // clear the drag flag
+		canMouseX = parseInt(e.clientX-offsetX);
+		canMouseY = parseInt(e.clientY-offsetY);
+		// clear the drag flag
       isDragging = false;
     }
 
     function handleMouseOut(e){//滑鼠移開的函數
-      canMouseX = parseInt(e.clientX-offsetX);
-      canMouseY = parseInt(e.clientY-offsetY);
-      // user has left the canvas, so clear the drag flag
-      //isDragging=false;
+		canMouseX = parseInt(e.clientX-offsetX);
+		canMouseY = parseInt(e.clientY-offsetY);
+		// user has left the canvas, so clear the drag flag
+		//isDragging=false;
     }
 
     function handleMouseMove(e){//滑鼠移動的event
-      canMouseX = parseInt(e.clientX-offsetX);
-      canMouseY = parseInt(e.clientY-offsetY);
-      // if the drag flag is set, clear the canvas and draw the image
-      if(isDragging){ //當拖拉為True時
-          	ctx.clearRect(0,0,canvasWidth,canvasHeight); //移除canvas起始的內容
-			var profileIMG = document.getElementById("preview");//抓html裡預載入的照片
-			profileIMG.crossOrigin = "Anonymous"; // 這務必要做，為了讓Facebook的照片能夠crossdomain傳入到你的頁面，CORS Policy請參考https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image 
-			//canvas.width = profileIMG.width;//設定canvas的大小需符合profileimg的大小
-			//canvas.height = profileIMG.height;
-			ctx.drawImage(profileIMG,canMouseX,canMouseY);//從XY軸0，0值開始畫如profileimg
+		canMouseX = parseInt(e.clientX-offsetX);
+		canMouseY = parseInt(e.clientY-offsetY);
+		// if the drag flag is set, clear the canvas and draw the image
+		if(isDragging){ //當拖拉為True時
+			ctx.clearRect(0,0,canvasWidth,canvasHeight); //移除canvas起始的內容
 			ctx.drawImage(img3,171,400); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
 			ctx.drawImage(img2,0,0); //劃入img2
 			var inputedText = $('#inputed').val();//抓取頁面inputed ID的內容
 			ctx.fillStyle = "black"; //字體顏色
 			ctx.font='20px "微軟正黑體"'; //字體大小和字形
 			ctx.fillText(inputedText, canMouseX-1/2,canMouseY-30/2); //字體也可以依據滑鼠游標移動，所輸入的值可自行調整，若不想移動輸入的字體，可以把它改成（inputedText,0,0)X Y軸 0，0的位置
-      }
+		}
     }
 
 	//抓取滑鼠移動的event
@@ -149,13 +149,6 @@ window.fbAsyncInit = function () {//facebook init
 
 }; //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<init end
 
-
-
-   
-
-
-
-
 //LOAD FACEBOOK SDK ASYNC，這是基本的東西，應該不用多說了吧
 (function (d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
@@ -167,10 +160,6 @@ window.fbAsyncInit = function () {//facebook init
     js.src = "//connect.facebook.net/en_US/sdk.js"; 
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
-
-
-
-
 
 // Post a BASE64 Encoded PNG Image to facebook，以下程式為把照片po到facebook的方法，基本上這樣就可以不用動了，但思考authToken該怎麼拿到，因為這裡我並沒有把使用者登入的token載入到這函數內，所以它是不會得到token的
 function PostImageToFacebook(authToken) {
@@ -210,9 +199,6 @@ function PostImageToFacebook(authToken) {
         console.log(e);//錯誤訊息的log
     }
 }
-
-
-
 
 // Convert a data URI to blob把影像載入轉換函數
 function dataURItoBlob(dataURI) {
