aeaf5e1d195943030cf71859f03377ebd5fd9e03
frank00125 <frank9732@gmail.com>
2014-06-09 20:18:36 +0800

    Version 1.06.10 partially debugged

--- js/script.js
+++ js/script.js
@@ -95,8 +95,8 @@ window.fbAsyncInit = function () {//facebook init
 	var isDrawed = false;
 
     function handleMouseDown(e){//滑鼠按下的函數
-		canMouseX = parseInt(e.clientX-offsetX);//抓滑鼠游標X
-		canMouseY = parseInt(e.clientY-offsetY);//抓滑鼠游標y
+		canMouseX = parseInt(e.clientX);//抓滑鼠游標X
+		canMouseY = parseInt(e.clientY);//抓滑鼠游標y
 		if(!isDrawed){
 			ctx.clearRect(0,0,canvasWidth,canvasHeight); //移除canvas起始的內容
 			ctx.drawImage(img3,171,400); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
@@ -106,7 +106,7 @@ window.fbAsyncInit = function () {//facebook init
 			ctx.drawImage(profileIMG,canMouseX,canMouseY);//從XY軸0，0值開始畫如profileimg
 			ctx.fillStyle = "black"; //字體顏色
 			ctx.font='20px "微軟正黑體"'; //字體大小和字形
-			ctx.fillText(inputed, 280, 430); //字體也可以依據滑鼠游標移動，所輸入的值可自行調整，若不想移動輸入的字體，可以把它改成（inputedText,0,0)X Y軸 0，0的位置
+			ctx.fillText(inputed, 280, 445); //字體也可以依據滑鼠游標移動，所輸入的值可自行調整，若不想移動輸入的字體，可以把它改成（inputedText,0,0)X Y軸 0，0的位置
 			isDrawed = true;
 		}
 		
@@ -146,7 +146,7 @@ window.fbAsyncInit = function () {//facebook init
 			//var inputedText = $('#inputed').val();//抓取頁面inputed ID的內容
 			ctx.fillStyle = "black"; //字體顏色
 			ctx.font='20px "微軟正黑體"'; //字體大小和字形
-			ctx.fillText(inputed, 280, 430); //字體也可以依據滑鼠游標移動，所輸入的值可自行調整，若不想移動輸入的字體，可以把它改成（inputedText,0,0)X Y軸 0，0的位置	
+			ctx.fillText(inputed, 280, 445); //字體也可以依據滑鼠游標移動，所輸入的值可自行調整，若不想移動輸入的字體，可以把它改成（inputedText,0,0)X Y軸 0，0的位置	
 		}
     }
 
